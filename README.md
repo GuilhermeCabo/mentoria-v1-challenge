10fcd169a62a60cd9763b9067204b65532f9e30e
GuilhermeCabo <guilherme.pcabo@gmail.com>
2022-10-25 17:05:15 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## FinAPI  - FINANCEIRO
+
+### REQUISITOS
+
+- [] Dever ser possível criar uma conta
+- [] Deve ser possível buscar o extrato bancário do cliente
+- [] Deve ser possível realizar um depósito
+- [] Deve ser possível realizar um saque
+- [] Deve ser possível buscar o extrato bancário do cliente por data
+- [] Deve ser possível atualizar dados da conta do cliente
+- [] Deve ser possível obter dados da conta do cliente
+- [] Deve ser possível deletar uma conta
+
+
+## Regras de Negócio
+
+- [] Não deve ser possível cadastrar uma conta com CPF já cadastrado
+- [] Não deve ser possível fazer depósito em uma conta não existente
+- [] Não deve ser possível buscar extrato de uma conta não existente
+- [] Não deve ser possível fazer saque em uma conta não existente
+- [] Não deve ser possível excluir uma conta não existente
+- [] Não deve ser possível fazer saque quando o saldo for insuficiente
---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
+
+/*
+
+Statement: List of all balance changes, like credit and debit (Extrato, in pt-br)
+
+customer = {
+  cpf: string;
+  name: string;
+  statement: {
+    description: string,
+    amount: number,
+    created_at: date,
+    type: "credit" | "debit",
+  }
+}
+
+*/
+
+const customers = [];
+
+function verifyIfAccountCpfExists(request, response, next) {
+  // TODO
+
+  // Use this hint wisely -> require.customer = customer
+
+  return next();
+}
+
+function getBalance(statement) {
+  // TODO: Show the current balance of the customer using it's statement
+
+  return balance;
+}
+
+app.post('/account', (request, response) => {
+  // TODO: Create a new customer account
+});
+
+app.get('/statement', (request, response) => {
+  // TODO: List all operations fron a customer
+});
+
+app.post('/deposit', (request, response) => {
+ // TODO: add a deposit (credit) to the statement list
+});
+
+app.post('/withdraw', (request, response) => {
+  // TODO: add a withdraw (debit) to the statement list 
+});
+
+app.listen(3333, console.log('Server has started...'));
